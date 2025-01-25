849e4d15b262422340b3117cbabeb7800c5c3242
Codkashh <as3733542@gmail.com>
2025-01-25 18:16:40 +0530

    bg file for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-
+import Background from './components/background'
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800 '>
-      <div className='w-full top-[5%] py-10 flex justify-center text-zinc-600 text-xl font-semibold'>Documents</div>
-      <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] leading-none tracking-tighter font-semibold text-zinc-900 text-[13vw]'>
-        Docs.
-      </h1>
+      <Background />
     </div>
   )
 }
