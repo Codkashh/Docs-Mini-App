965df15b1c23d5a24c91d21b5156d347c81d84ef
Codkashh <as3733542@gmail.com>
2025-01-26 23:38:22 +0530

    updation

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Card from './Card'
 function Foreground() {
+    const data = [
+        {
+            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            filesize:".10mb",
+            close: true,
+            tag: {isOpen:true, tagTitle:"Download Now", tagColor:"green" },
+        },
+    ]
   return (
     <>
         <div className='fixed top-0 left-0  z-[3] w-full h-full'>
